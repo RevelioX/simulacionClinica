443e7ed8f211a15ed0573a73a17d8e26e5a81488
Mariano Brarda <64959782+RevelioX@users.noreply.github.com>
2024-06-04 00:20:51 -0300

    added generators

---
+++ src/main/java/com/example/simuladorclinica/SimuladorClinicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.simuladorclinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorClinicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimuladorClinicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simuladorclinica/SimuladorClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simuladorclinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorClinicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
