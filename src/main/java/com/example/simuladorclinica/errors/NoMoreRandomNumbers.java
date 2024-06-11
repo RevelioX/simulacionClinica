4629a6133b05697463f3dc2f3dcf5f36fddcffb7
Mariano Brarda <64959782+RevelioX@users.noreply.github.com>
2024-06-11 18:09:54 -0300

    added generators

---
+++ src/main/java/com/example/simuladorclinica/errors/NoMoreRandomNumbers.java
@@ -0,0 +1,4 @@
+package com.example.simuladorclinica.errors;
+
+public class NoMoreRandomNumbers extends Error {
+}
---
+++ src/main/java/com/example/simuladorclinica/logic/Controller.java
@@ -0,0 +1,4 @@
+package com.example.simuladorclinica.logic;
+
+public class Controller {
+}
