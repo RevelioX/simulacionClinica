c533b74b0f2fc148d314a9b57b19c1df8edf1647
Agustin Nicolas Colodny <agustincolodny@gmail.com>
2024-06-27 10:08:43 -0300

    agregue acumulador pacientes emergencias

--- src/main/java/com/example/simuladorclinica/ecuDiferencial/ResolverEcDiferencial.java
+++ src/main/java/com/example/simuladorclinica/ecuDiferencial/ResolverEcDiferencial.java
@@ -4,6 +4,7 @@ public class ResolverEcDiferencial {
     public static double resolverEcuacion(String ecDiferencial){
 
         RungeKutta rungekutta = new RungeKutta(0,1,0.1f,ecDiferencial);
+
         double resultado = (double) rungekutta.calcularRunge();
 
         return resultado;
